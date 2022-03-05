ec9576bf65107fadcd44731b046bca7ad079b3ea
Robe <robertruben98@gmail.com>
2022-03-05 17:40:33 -0500

    Inserting the date

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, g, request
 import sqlite3
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -21,8 +22,18 @@ def close_db(error):
     if hasattr(g, 'sqlite_db'):
         g.sqlite_db.close()
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
+    db = get_db()
+    if request.method == 'POST':
+        date = request.form['date'] # assuming the date is in YYYY-MM-DD format
+
+        dt = datetime.strptime(date, '%Y-%m-%d')
+        database_date = datetime.strftime(dt, '%Y%m%d')
+
+        db.execute('insert into log_date (entry_date) values (?)', [database_date])
+        db.commit()
+
     return render_template('home.html')
 
 
