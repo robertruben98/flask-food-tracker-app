31864f78218f446dfec1681dd22846462130a0d5
Robe <robertruben98@gmail.com>
2022-03-05 23:52:29 -0500

    Sum Totals Per Day

--- app.py
+++ app.py
@@ -35,7 +35,11 @@ def index():
         db.execute('insert into log_date (entry_date) values (?)', [database_date])
         db.commit()
 
-    cur = db.execute('select entry_date from log_date order by entry_date desc')
+    cur = db.execute('select log_date.entry_date, sum(food.protein) as protein, '
+                     'sum(food.carbohydrates) as carbohydrates, sum(food.fat) as fat, sum(food.calories) as calories '
+                     'from log_date join food_date on food_date.log_date_id = log_date.id '
+                     'join food on food.id = food_date.food_id '
+                     'group by log_date.id order by log_date.entry_date desc')
     results = cur.fetchall()
 
     date_results = []
@@ -43,6 +47,10 @@ def index():
         single_date = {}
 
         single_date['entry_date'] = i['entry_date']
+        single_date['protein'] = i['protein']
+        single_date['carbohydrates'] = i['carbohydrates']
+        single_date['fat'] = i['fat']
+        single_date['calories'] = i['calories']
 
         d = datetime.strptime(str(i['entry_date']), '%Y%m%d')
         single_date['pretty_date'] = datetime.strftime(d, '%B %d, %Y')
